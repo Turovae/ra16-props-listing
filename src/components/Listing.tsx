7ee999a9b562db807d62124ecbe9e6de3f61fda4
TurovaE <ekaterina.turova@mail.ru>
2023-05-19 11:43:05 +0700

    add Offer component

--- src/components/Listing.tsx
+++ src/components/Listing.tsx
@@ -1,24 +1,30 @@
 import Offer from "./Offer";
 
 interface ItemProps {
-    listing_id: number,
+  listing_id: number,
+  state?: string,
+  url: string,
+  MainImage: { url_570xN: string },
+  title: string,
+  price: string,
+  currency_code: string,
+  quantity: string,
 }
 
 interface ListingProps {
-    items?: Array<ItemProps>
+  items?: Array<ItemProps>
 }
 
 function Listing(props: ListingProps) {
-    const { items = [] } = props;
+  const { items = [] } = props;
 
-    console.log(items);
+  console.log(items);
 
-    return (
-        <div className="item-list">
-            <h3>I am listing component</h3>
-            {items.map((item) => <Offer key={item.listing_id} item={item} />)}
-        </div>
-    );
+  return (
+    <div className="item-list">
+      {items.map((item) => item.state !== 'removed' ? <Offer key={item.listing_id} item={item} /> : null)}
+    </div>
+  );
 }
 
 export default Listing;
--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,14 +1,34 @@
+interface itemProps {
+  url: string,
+  title: string,
+  MainImage: { url_570xN: string },
+  price: string,
+  currency_code: string,
+  quantity: string,
+}
+
 interface offerProps {
-  // props: object,
-  item: object;
+  item: itemProps,
 }
 
 function Offer(props: offerProps) {
   console.log(props);
+  const { url, MainImage, title, price, currency_code, quantity } = props.item;
 
   return (
     <>
-      <h4>I am offer</h4>
+      <div className="item">
+        <div className="item-image">
+          <a href={url}>
+            <img src={MainImage.url_570xN} alt="" />
+          </a>
+        </div>
+        <div className="item-details">
+          <p className="item-title">{title}</p>
+          <p className="item-price">{price}{currency_code}</p>
+          <p className="item-quantity level-high">{quantity}</p>
+        </div>
+      </div>
     </>
   );
 
